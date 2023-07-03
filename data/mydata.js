25731cfade91ad86016a62a7b32c8129620d3a08
dunjap2003 <138401545+dunjap2003@users.noreply.github.com>
2023-07-03 12:22:59 +0200

    Add files via upload

---
+++ data/mydata.js
@@ -0,0 +1,207 @@
+const data = {
+   "website": "Healthy kitchen",
+   "categories": [
+       { 
+           "name" : "Lisnato povrće",
+           "products" : [
+               { 
+                   "name" : "Prokulice", "image" : "/images/prokulice.jpg", "counter" : 0
+               },
+               { 
+                   "name" : "Blitva", "image" : "/images/blitva.jpg", "counter" : 0
+               },                
+               { 
+                   "name" : "Kelj", "image" : "/images/kelj.jpg",  "counter" : 0
+               },
+               {
+                  "name" : "Zelena salata", "image" : "/images/zelenaSalata.jpg", "counter" : 0
+               },
+               {
+                  "name" : "Špinat", "image" : "/images/spinat.jpg", "counter" : 0
+               }
+           ]
+       },
+       { 
+           "name" : "Korjenasto povrće",
+           "products" : [
+               { 
+                  "name" : "Đumbir", "image" : "/images/djumbir.jpg", "counter" : 0
+               },
+               { 
+                     "name" : "Krumpir", "image" : "/images/krumpir.jpg", "counter" : 0
+               },                
+               { 
+                     "name" : "Batat", "image" : "/images/batat.jpg", "counter" : 0
+               },
+               {
+                  "name" : "Celer", "image" : "/images/celer.jpg", "counter" : 0
+               },
+               {
+                  "name" : "Peršin", "image" : "/images/persin.jpg", "counter" : 0
+               }
+           ]
+       },
+       {
+            "name" : "Lukovičasto povrće",
+            "products" : [
+               { 
+                  "name" : "Crveni luk", "image" : "/images/crveniluk.jpg", "counter" : 0
+               },
+               { 
+                     "name" : "Žuti luk", "image" : "/images/zutiLuk.jpg", "counter" : 0
+               },                
+               { 
+                     "name" : "Češnjak", "image" : "/images/cesnjak.jpg", "counter" : 0
+               },
+               {
+                  "name" : "Poriluk", "image" : "/images/poriluk.jpg", "counter" : 0
+               },
+               {
+                  "name" : "Luk vlasac", "image" : "/images/lukvlasac.jpg", "counter" : 0
+               }
+            ]
+       },
+       {
+         "name" : "Začini",
+         "products" : [
+             { 
+                "name" : "Kurkuma", "image" : "/images/kurkuma.jpg", "counter" : 0
+             },
+             { 
+                   "name" : "Curry", "image" : "/images/curry.jpg", "counter" : 0
+             },                
+             { 
+                   "name" : "Čili u prahu", "image" : "/images/cili.jpg", "counter" : 0
+             },
+             {
+                "name" : "Crni papar", "image" : "/images/crnipapar.jpg", "counter" : 0
+             },
+             {
+                "name" : "Cimet", "image" : "/images/cimet.jpg", "counter" : 0
+             }
+         ]
+       },
+       {
+         "name" : "Agrumi",
+         "products" : [
+             { 
+                "name" : "Naranče", "image" : "/images/naranca.jpg", "counter" : 0
+             },
+             { 
+                "name" : "Mandarine", "image" : "/images/mandarina.jpg", "counter" : 0
+             },                
+             { 
+                "name" : "Limun", "image" : "/images/limun.jpg", "counter" : 0
+             },
+             {
+                "name" : "Limeta", "image" : "/images/limeta.jpg", "counter" : 0
+             },
+             {
+                "name" : "Crveni grejp", "image" : "/images/crvenigrejp.jpg", "counter" : 0
+             }
+         ]
+       },
+       {
+         "name" : "Bobičasto voće",
+         "products" : [
+             { 
+                "name" : "Jagode", "image" : "/images/jagode.jpg", "counter" : 0
+             },
+             { 
+                "name" : "Borovnice", "image" : "/images/borovnice.jpg", "counter" : 0
+             },                
+             { 
+                "name" : "Kupine", "image" : "/images/kupine.jpg", "counter" : 0
+             },
+             {
+                "name" : "Crni ribiz", "image" : "/images/crniRibiz.jpg", "counter" : 0
+             },
+             {
+                "name" : "Maline", "image" : "/images/maline.jpg", "counter" : 0
+             }
+         ]
+       },
+       {
+         "name" : "Jabučasto voće",
+         "products" : [
+             { 
+                "name" : "Jabuke", "image" : "/images/jabuke.jpg", "counter" : 0
+             },
+             { 
+                "name" : "Kruške", "image" : "/images/kruska.jpg", "counter" : 0
+             },                
+             { 
+                "name" : "Dunje", "image" : "/images/dunje.jpg", "counter" : 0
+             },
+             {
+                "name" : "Mušmule", "image" : "/images/musmula.jpg", "counter" : 0
+             },
+             {
+                "name" : "Šipak", "image" : "/images/sipak.jpg", "counter" : 0
+             }
+         ]
+       },
+       {
+         "name" : "Tropsko voće",
+         "products" : [
+             { 
+                "name" : "Pitaja", "image" : "/images/pitaja.jpg", "counter" : 0
+             },
+             { 
+                "name" : "Ananas", "image" : "/images/ananas.jpg", "counter" : 0
+             },                
+             { 
+                "name" : "Mango", "image" : "/images/mango.jpg", "counter" : 0
+             },
+             {
+                "name" : "Papaja", "image" : "/images/papaja.jpg", "counter" : 0
+             },
+             {
+                "name" : "Banane", "image" : "/images/banana.jpg", "counter" : 0
+             }
+         ]
+       },
+       {
+         "name" : "Koštuničavo voće",
+         "products" : [
+             { 
+                "name" : "Šljive", "image" : "/images/sljiva.jpg", "counter" : 0
+             },
+             { 
+                "name" : "Nektarine", "image" : "/images/nektarina.jpg", "counter" : 0
+             },                
+             { 
+                "name" : "Višnje", "image" : "/images/visnja.jpg", "counter" : 0
+             },
+             {
+                "name" : "Trešnje", "image" : "/images/tresnja.jpg", "counter" : 0
+             },
+             {
+                "name" : "Marelice", "image" : "/images/marelica.jpg", "counter" : 0
+             }
+         ]
+       },
+       {
+         "name" : "Orašasti plodovi",
+         "products" : [
+             { 
+                "name" : "Bademi", "image" : "/images/badem.jpg", "counter" : 0
+             },
+             { 
+                "name" : "Kesteni", "image" : "/images/kesten.jpg", "counter" : 0
+             },                
+             { 
+                "name" : "Lješnjaci", "image" : "/images/ljesnjak.jpg", "counter" : 0
+             },
+             {
+                "name" : "Pistacije", "image" : "/images/pistacija.jpg", "counter" : 0
+             },
+             {
+                "name" : "Orasi", "image" : "/images/orah.jpg", "counter" : 0
+             }
+         ]
+       }
+   ]
+}
+
+module.exports = data;
